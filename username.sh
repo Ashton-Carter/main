f9026dd329c97b80d17f5122f44969651acfea0c
Ashton Carter <ascarter@chapman.edu>
2022-11-06 14:22:17 -0800

    Updated

--- username.sh
+++ username.sh
@@ -4,7 +4,7 @@
 # date:
 echo -n "Enter a username.  Can only contain lowercase letters, underscores, and numbers.  Must be between 3 and 12 characters long"
 read -r USERNAME
-while echo "$USERNAME" | grep -E -v "^[a-z_0-9]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | grep -E -v "^[a-z][a-z_0-9]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid usernmae"
 	echo -n "Enter a username: "
